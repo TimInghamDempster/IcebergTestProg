d3210c51e8df93734225ba7d219a15b865a7465f
Tim Ingham-Dempster <tim_ingham_dempster@hotmail.co.uk>
2015-12-10 17:02:33 +0000

    [TID] Test environment variable and file output

--- IcebergTestProg/IcebergTestProg.cpp
+++ IcebergTestProg/IcebergTestProg.cpp
@@ -1,8 +1,38 @@
 // IcebergTestProg.cpp : Defines the entry point for the console application.
 //
+#include <stdio.h>
+#include <thread>
 
 int main(int argc, char* argv[])
 {
+	FILE* newFile = fopen("testFile.txt",  "a");
+
+	for(int i = 0; i < 10; i++)
+	{
+		if(newFile != NULL)
+		{
+			break;
+		}
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));		
+	}
+
+	if(newFile != NULL)
+	{
+		char* env = getenv("SGE_TASK_ID");
+
+		if(env != NULL)
+		{
+			int numChars = fprintf(newFile, "%s %s", env, "\n");
+		}
+		else
+		{
+			int numChars = fprintf(newFile, "%s", "Failed to get array variable \n");
+		}
+
+		fclose(newFile);
+	}
+
 	return 0;
 }
 
